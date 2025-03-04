816dcdd51d530d5e217e3d3b61188502f166d5a5
hutslip11 <hutslip@gmail.com>
2025-03-03 20:37:22 -0500

    Initial Commit

---
+++ lostNfoUnd/lostNfoUndApp.swift
@@ -0,0 +1,17 @@
+//
+//  lostNfoUndApp.swift
+//  lostNfoUnd
+//
+//  Created by Neel Chandwani on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct lostNfoUndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lostNfoUndTests/lostNfoUndTests.swift
@@ -0,0 +1,17 @@
+//
+//  lostNfoUndTests.swift
+//  lostNfoUndTests
+//
+//  Created by Neel Chandwani on 3/3/25.
+//
+
+import Testing
+@testable import lostNfoUnd
+
+struct lostNfoUndTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
